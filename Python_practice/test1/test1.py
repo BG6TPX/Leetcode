48c7ee35a1028286555e03ca47f1570af6c33fe4
zyZhang <1450665003@qq.com>
2024-10-13 16:31:05 +0800

    fix(practice): update python

--- Python_practice/test1/test1.py
+++ Python_practice/test1/test1.py
@@ -2,4 +2,12 @@ from os import *
 import keyword
 
 
-keyword.kwlist
+class myclass:
+    i = 123456
+    def f(self):
+        return "hello world"
+
+x = myclass()
+
+print("MyClass 类的属性 i 为：", x.i)
+print("MyClass 类的方法 f 输出为：", x.f())
